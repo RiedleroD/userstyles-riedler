6c73e2ae5a88b8313a45816a4689f57d286807cd
RiedleroD <github@riedler.wien>
2023-03-04 13:13:08 +0100

    Merged Spacious Mastodon 1.2.1
    commit 6f42dd3912b2954d469f21ec629f282f77528df3
    Author: RiedleroD <github@riedler.wien>
    Date:   Sat Mar 4 13:09:57 2023 +0100
    
        bumped to 1.2.1
    
    commit 99c806cc58f049d8324afcadda8c601a1da55574
    Author: RiedleroD <github@riedler.wien>
    Date:   Sat Mar 4 13:09:09 2023 +0100
    
        status card don't trigger sidebar mode anymore
    
    commit 610fd244f175ed5a1aa736d81eaf22e2a923b73e
    Author: RiedleroD <github@riedler.wien>
    Date:   Sat Mar 4 13:05:21 2023 +0100
    
        added fosstodon and mastodon.social to supported instances
    
    commit b9a3221ea39d088d35b96aec9f193afbb33bbac1
    Author: RiedleroD <github@riedler.wien>
    Date:   Sat Mar 4 13:04:45 2023 +0100
    
        checking for modifications inside posts as well now
    
    commit afa8e1a9a269fdeb89fe279986ec0e1959d657b2
    Author: RiedleroD <github@riedler.wien>
    Date:   Sat Mar 4 12:38:37 2023 +0100
    
        unignored js files
    
    commit 4a5987513610e8aef2a947e4a70dc9ab3253b63b
    Author: RiedleroD <github@riedler.wien>
    Date:   Sat Mar 4 12:18:09 2023 +0100
    
        synchronized versions between userscript and userstyle
    
    commit 7ad437a947e8a87435574d6274aa1034dac1ece6
    Author: RiedleroD <github@riedler.wien>
    Date:   Sat Mar 4 12:17:43 2023 +0100
    
        added companion userscript
    
    commit 8b67ee02df081286d77bce2f79c19e774fce25cd
    Author: RiedleroD <github@riedler.wien>
    Date:   Sat Mar 4 12:09:45 2023 +0100
    
        preparing for companion userscript
    
    commit b29f5738da1b40f16453bbf5e72b2018e198735b
    Author: RiedleroD <github@riedler.wien>
    Date:   Sat Mar 4 12:07:11 2023 +0100
    
        fixed post times breaking line in general

---
+++ spacious_mastodon.user.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name         Spaceous Mastodon Helper
+// @namespace    github.com/RiedleroD/userstyles-riedler
+// @version      1.2.1
+// @description  companion script for spaceous mastodon userstyle
+// @author       Riedler
+// @match        https://mas.to/*
+// @match        https://fosstodon.org/*
+// @match        https://mastodon.social/*
+// @icon         https://mas.to/favicon.ico
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("me alive");
+
+    function checkSideBar(post){
+        let content = post.getElementsByClassName('status__content')[0];
+        //get minimum height by getting 15em in px
+        let divisor = 12*parseFloat(getComputedStyle(post).fontSize);
+        let actionbar = post.getElementsByClassName('status__action-bar')[0];
+        if(content.clientHeight>=divisor){
+            actionbar.classList.add('sm__force_sidebar');
+        }else{
+            //if we previously added it and it changed in the meantime
+            actionbar.classList.remove('sm__force_sidebar');
+        }
+    }
+    function isPost(element){
+        return element.classList!=undefined && element.classList.contains('status');
+    }
+
+    const target = document.getElementById('mastodon');
+    const obconf = { attributes: false, childList: true, subtree: true };
+
+    const callback = (mutationList, observer) => {
+        for(const mutation of mutationList){
+            for(let node of mutation.addedNodes){
+                if(isPost(node)){
+                    checkSideBar(node);
+                }else{
+                    //checking parents for posts
+                    do{
+                        node=node.parentNode;
+                        if(isPost(node)){
+                            checkSideBar(node);
+                            return;
+                        }
+                    }while(node.parentNode!=undefined);
+                    //checking children for posts
+                    for(const e of node.getElementsByClassName('status')){
+                        checkSideBar(e);
+                    }
+                }
+            }
+        }
+    };
+
+    const observer = new MutationObserver(callback);
+    observer.observe(target, obconf);
+})();
